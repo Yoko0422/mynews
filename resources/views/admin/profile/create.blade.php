cfb094b8f3d05e5994559191def7e504833a8ed3
Yoko0422 <cerise2@echecs.sakura.ne.jp>
2020-04-10 15:45:04 +0000

    Laravel課題20

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,7 +1,8 @@
-{{-- layouts/profile.blade.phpを読み込む --}}
+<{{-- layouts/profile.blade.phpを読み込む --}}
 @extends('layouts.profile')
 
-{{-- profile.blade.phpの@yield('title')に'プロフィール'を埋め込む --}}
+
+{{-- profile.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
 @section('title', 'Myプロフィール')
 
 {{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
@@ -9,7 +10,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>My プロフィール</h2>
+                <h2>Myプロフィール</h2>
             </div>
         </div>
     </div>
