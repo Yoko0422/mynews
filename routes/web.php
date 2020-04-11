dd45f49086c8380f58acb8203bc5e2f01fb522f1
Yoko0422 <cerise2@echecs.sakura.ne.jp>
2020-04-11 13:52:06 +0000

    PHP/Lrabel12課題

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,6 @@ Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
     Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
     Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
-
 });
 
 
@@ -30,6 +29,9 @@ Route::get('XXX', 'AAAController@bbb');
 
 //課題18-4:admin/profile/createにアクセスしたらProfileControllerのadd Action に、admin/profile/editにアクセスしたらProfileControllerのedit Action に割り当てる
 
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
