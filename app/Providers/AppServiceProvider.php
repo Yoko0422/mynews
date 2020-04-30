3338e72b98e5194ab7d570e6594c434e69486088
Yoko0422 <cerise2@echecs.sakura.ne.jp>
2020-04-30 12:32:42 +0000

    4/30 Heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+       // PHP Haroku
+    if (\App::environment('production')) {
+        \URL::forceScheme('https');
+    }
     }
 }
